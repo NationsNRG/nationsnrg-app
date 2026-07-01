0220870fcc7c6adbbb95cbc0e328118ac373bf9b
NationsNRG <nationsnrgconsultancy@gmail.com>
2026-07-01 01:33:35 -0400

    Remove remote Google font build dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from 'next'
 import Script from 'next/script'
 import Link from 'next/link'
-import { Geist, Geist_Mono } from 'next/font/google'
 
 import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'NationsNRG',
@@ -29,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
+        className="antialiased bg-black text-white"
     >
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-33FVXR2NX8"
